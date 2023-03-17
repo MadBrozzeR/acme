75d229c52442f40871915d7d630fa7f290bf0cd5
Sergei Zavgorodnii <zavgorodny@uchi.ru>
2023-03-18 01:14:28 +0300

    move test server to separate module

--- test/index.js
+++ test/index.js
@@ -1,57 +1,13 @@
 const http = require('http');
 const { TestSuit } = require('./suit/index.js');
 const { Acount } = require('../index.js');
+const { createServer } = require('./server.js');
 
-const mockData = {
-  requests: [],
-  listener: null,
-  attach: function (listener) {
-    this.listener = listener;
-  },
-  collect: function () {
-    const result = this.requests;
-    this.requests = [];
-    return result;
-  }
-};
-
-const server = http.createServer(function (request, response) {
-  const data = [];
-  let length = 0;
-  request.on('data', function (chunk) {
-    data.push(chunk);
-    length += chunk.length;
-  });
-  request.on('end', function () {
-    try {
-      const requestData = {
-        method: request.method,
-        path: request.url,
-        headers: request.headers,
-        data: Buffer.concat(data, length),
-      };
-      mockData.requests.push(requestData);
-      const responseData = mockData.listener && mockData.listener(requestData);
-      if (response) {
-        response.writeHead(responseData.status || 200, responseData.headers || {});
-        const data = typeof responseData.data === 'string'
-          ? responseData.data
-          : responseData.data
-            ? JSON.stringify(responseData.data)
-            : undefined
-        response.end(data);
-      } else {
-        response.end();
-      }
-    } catch (error) {
-      response.end();
-    }
-  });
-}).listen(5084, function () {
+createServer(5084).then(function (mockData) {
   new TestSuit({ mocks: mockData }).test({
     'Api module': require('./api.js'),
   }).result(function (unsuccessfull) {
-    server.close();
+    mockData.server.close();
 
     if (unsuccessfull) {
       process.exit(1);
---
+++ test/server.js
@@ -0,0 +1,56 @@
+const http = require('http');
+
+function createServer (port) {
+  return new Promise(function (resolve, reject) {
+    const mockData = {
+      requests: [],
+      listener: null,
+      attach: function (listener) {
+        this.listener = listener;
+      },
+      collect: function () {
+        const result = this.requests;
+        this.requests = [];
+        return result;
+      }
+    };
+
+    mockData.server = http.createServer(function (request, response) {
+      const data = [];
+      let length = 0;
+      request.on('data', function (chunk) {
+        data.push(chunk);
+        length += chunk.length;
+      });
+      request.on('end', function () {
+        try {
+          const requestData = {
+            method: request.method,
+            path: request.url,
+            headers: request.headers,
+            data: Buffer.concat(data, length),
+          };
+          mockData.requests.push(requestData);
+          const responseData = mockData.listener && mockData.listener(requestData);
+          if (response) {
+            response.writeHead(responseData.status || 200, responseData.headers || {});
+            const data = typeof responseData.data === 'string'
+              ? responseData.data
+              : responseData.data
+                ? JSON.stringify(responseData.data)
+                : undefined
+            response.end(data);
+          } else {
+            response.end();
+          }
+        } catch (error) {
+          response.end();
+        }
+      });
+    }).listen(port, function () {
+      resolve(mockData);
+    });
+  });
+}
+
+module.exports = { createServer };
